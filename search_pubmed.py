e88f7a97518d42351bb6439fe74ecceb6a33bf04
tydymy <tdmyers@eng.ucsd.edu>
2023-05-02 14:22:52 -0700

    Update search_pubmed.py

--- search_pubmed.py
+++ search_pubmed.py
@@ -22,3 +22,21 @@ def search_pubmed(keywords):
         id_list.extend(record["IdList"])
         
     return id_list
+
+# define a function to download a single file from the S3 bucket
+def download_file(file_name):
+    # create an S3 client object
+    s3 = boto3.client('s3')
+    bucket_name = 'pmc-oa-opendata'
+    prefix = 'oa_comm/txt/all/'
+    
+    try:
+        # download the file from the S3 bucket
+        s3.download_file(bucket_name, prefix + file_name, os.path.join('text', file_name))
+        #s3.get_object(Bucket=bucket_name, Key=prefix+file_name)
+        #print(f"File {file_name} downloaded from S3 bucket")
+        return 1
+    except Exception as e:
+        # if the file is not available in the S3 bucket, print the error message
+        #print(f"File {file_name} not available in S3 bucket. Error message: {e}")
+        return 0
